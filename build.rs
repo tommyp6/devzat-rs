f23aa902ecece42e519c50cba3af5d403ee51f5e
Tommy <43862943+tommyp6@users.noreply.github.com>
2022-06-23 17:49:54 +0200

    Revert build.rs

--- build.rs
+++ build.rs
@@ -1,7 +1,4 @@
 fn main() -> Result<(), Box<dyn std::error::Error>> {
-    if !std::env::var("DOCS_RS").is_ok() {
-        tonic_build::compile_protos("proto/devzat.proto")?;
-    }
-
+    tonic_build::compile_protos("proto/devzat.proto")?;
     Ok(())
 }
